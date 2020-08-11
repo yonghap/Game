f7ff3bd31930c8243b21146a95e2964169e2fad1
Park Yong Ha <yonghapk@gmail.com>
2020-08-11 23:58:44 +0900

    Modify Header

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -1,22 +1,28 @@
 <template>
 	<header id="header" class="header">
-		<h1 class="logo">
-			<a href="#">
-				<img src="@/assets/images/logo.png" alt="Gameb">
-			</a>
-		</h1>
-		<nav id="nav" class="nav">
-			<a href="#">소개</a>
-			<a href="#">둘러보기</a>
-		</nav>
+		<div class="header__row">
+			<h1 class="logo">
+				<a href="#">
+					<img src="@/assets/images/logo.png" alt="Gameb">
+				</a>
+			</h1>
+			<nav id="nav" class="nav">
+				<a href="#">소개</a>
+				<a href="#">둘러보기</a>
+			</nav>
+		</div>
 	</header>
 </template>
 <style lang="scss" scoped>
 	.header {
-		display:flex;
-		padding:10px 20px;
-		justify-content:space-between;
 		background:#171A21;
+		&__row {
+			display:flex;
+			max-width:1140px;
+			margin:0 auto;
+			padding:20px;
+			justify-content:space-between;
+		}
 	}
 	.logo {
 		a {
@@ -28,6 +34,7 @@
 		}
 	}
 	.nav {
+		align-items:center;
 		a {
 			color:#fff;
 		}
