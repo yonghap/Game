afe7ebbf626c7a737a353092fb3f72ceb4cf5c61
Park Yong Ha <yonghapk@gmail.com>
2020-08-14 23:59:44 +0900

    Add Tag

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -6,6 +6,88 @@
 				<Sidebar></Sidebar>
 				<div class="content__wrap">
 					<article class="article">
+						<div class="article__tag">
+							<Tag></Tag>
+						</div>
+						<ul class="list">
+							<li>
+								<a href="#">
+									<span class="list__image" :style="{'background-image': 'url(' + require('../assets/images/game1.jpg') + ')'}">
+
+									</span>
+									<span class="list__title">
+										Desperados 3
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="list__image" :style="{'background-image': 'url(' + require('../assets/images/game2.jpg') + ')'}">
+
+									</span>
+									<span class="list__title">
+										Desperados 3
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="list__image" :style="{'background-image': 'url(' + require('../assets/images/game3.jpg') + ')'}">
+
+									</span>
+									<span class="list__title">
+										Desperados 3
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="list__image" :style="{'background-image': 'url(' + require('../assets/images/game4.jpg') + ')'}">
+
+									</span>
+									<span class="list__title">
+										Desperados 3
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="list__image" :style="{'background-image': 'url(' + require('../assets/images/game5.jpg') + ')'}">
+
+									</span>
+									<span class="list__title">
+										Desperados 3
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="list__image" :style="{'background-image': 'url(' + require('../assets/images/game6.jpg') + ')'}">
+
+									</span>
+									<span class="list__title">
+										Desperados 3
+									</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="list__image" :style="{'background-image': 'url(' + require('../assets/images/game7.jpg') + ')'}">
+
+									</span>
+									<span class="list__title">
+										Desperados 3
+									</span>
+								</a>
+							</li>
+						</ul>
+						<div class="more">
+							<a href="#" class="btn-common">
+								MORE
+							</a>
+						</div>
+					</article>
+					<article class="article">
 						<h2 class="article__title">인기 게임</h2>
 						<ul class="list">
 							<li>
@@ -165,7 +247,6 @@
 							</a>
 						</div>
 					</article>
-					</article>
 				</div>
 			</div>
 		</section>
@@ -175,11 +256,13 @@
 <script>
 	import TheHeader from '@/components/TheHeader';
 	import Sidebar from '@/components/Sidebar';
+	import Tag from '@/components/Tag';
 
 	export default {
 		components: {
 			TheHeader,
-			Sidebar
+			Sidebar,
+			Tag
 		},
 		name: 'Search',
 		data() {
